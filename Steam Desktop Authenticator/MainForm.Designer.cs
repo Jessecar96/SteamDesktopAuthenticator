b259d099f25c69740482bfb52f33214a06b600ed
Jesse Cardone <jessecar96@gmail.com>
2015-11-26 22:48:19 -0500

    Login code generating works

--- Steam Desktop Authenticator/MainForm.Designer.cs
+++ Steam Desktop Authenticator/MainForm.Designer.cs
@@ -31,7 +31,14 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.btnSteamLogin = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtLoginToken = new System.Windows.Forms.TextBox();
+            this.pbTimeout = new System.Windows.Forms.ProgressBar();
+            this.listAccounts = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnSteamLogin
@@ -44,24 +51,80 @@
             this.btnSteamLogin.UseVisualStyleBackColor = true;
             this.btnSteamLogin.Click += new System.EventHandler(this.btnSteamLogin_Click);
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pbTimeout);
+            this.groupBox1.Controls.Add(this.txtLoginToken);
+            this.groupBox1.Location = new System.Drawing.Point(23, 100);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(305, 85);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Login Token";
+            // 
+            // txtLoginToken
+            // 
+            this.txtLoginToken.BackColor = System.Drawing.SystemColors.Window;
+            this.txtLoginToken.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLoginToken.Location = new System.Drawing.Point(6, 19);
+            this.txtLoginToken.Name = "txtLoginToken";
+            this.txtLoginToken.ReadOnly = true;
+            this.txtLoginToken.Size = new System.Drawing.Size(293, 35);
+            this.txtLoginToken.TabIndex = 0;
+            this.txtLoginToken.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // pbTimeout
+            // 
+            this.pbTimeout.Location = new System.Drawing.Point(6, 60);
+            this.pbTimeout.Name = "pbTimeout";
+            this.pbTimeout.Size = new System.Drawing.Size(293, 19);
+            this.pbTimeout.TabIndex = 1;
+            // 
+            // listAccounts
+            // 
+            this.listAccounts.FormattingEnabled = true;
+            this.listAccounts.Items.AddRange(new object[] {
+            "test",
+            "test"});
+            this.listAccounts.Location = new System.Drawing.Point(23, 195);
+            this.listAccounts.Name = "listAccounts";
+            this.listAccounts.Size = new System.Drawing.Size(305, 160);
+            this.listAccounts.TabIndex = 3;
+            this.listAccounts.SelectedValueChanged += new System.EventHandler(this.listAccounts_SelectedValueChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
             // MainForm
             // 
             this.AcceptButton = this.btnSteamLogin;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(351, 499);
+            this.ClientSize = new System.Drawing.Size(351, 378);
+            this.Controls.Add(this.listAccounts);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.btnSteamLogin);
             this.MaximizeBox = false;
             this.Name = "MainForm";
             this.Resizable = false;
             this.ShadowType = MetroFramework.Forms.MetroForm.MetroFormShadowType.SystemShadow;
             this.Text = "Steam Desktop Authenticator";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
         private System.Windows.Forms.Button btnSteamLogin;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ProgressBar pbTimeout;
+        private System.Windows.Forms.TextBox txtLoginToken;
+        private System.Windows.Forms.ListBox listAccounts;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Steam Desktop Authenticator/MainForm.cs
+++ Steam Desktop Authenticator/MainForm.cs
@@ -7,20 +7,79 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SteamAuth;
+using System.Diagnostics;
 
 namespace Steam_Desktop_Authenticator
 {
     public partial class MainForm : MetroFramework.Forms.MetroForm
     {
+        private SteamGuardAccount mCurrentAccount = null;
+        private SteamGuardAccount[] allAccounts;
+
+        private long steamTime = 0;
+        private int steamTimeLeft = 30;
+
         public MainForm()
         {
             InitializeComponent();
+            loadAccountsList();
+
+            pbTimeout.Maximum = 30;
+            pbTimeout.Minimum = 0;
+            pbTimeout.Value = 30;
         }
 
         private void btnSteamLogin_Click(object sender, EventArgs e)
         {
             LoginForm mLoginForm = new LoginForm();
             mLoginForm.ShowDialog();
+        }
+
+        private void listAccounts_SelectedValueChanged(object sender, EventArgs e)
+        {
+            // Triggered when list item is clicked
+            for (int i = 0; i < allAccounts.Length; i++)
+            {
+                SteamGuardAccount account = allAccounts[i];
+                if (account.AccountName == (string) listAccounts.Items[listAccounts.SelectedIndex])
+                {
+                    mCurrentAccount = account;
+                    loadAccountInfo();
+                }
+            }
+        }
+
+        private void loadAccountsList()
+        {
+            listAccounts.Items.Clear();
+
+            allAccounts = MobileAuthenticatorFileHandler.GetAllAccounts();
+            for (int i = 0; i < allAccounts.Length; i++)
+            {
+                SteamGuardAccount account = allAccounts[i];
+                listAccounts.Items.Add(account.AccountName);
+            }
+        }
+
+        private void loadAccountInfo()
+        {
+            if (mCurrentAccount != null)
+            {
+                txtLoginToken.Text = mCurrentAccount.GenerateSteamGuardCodeForTime(steamTime);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(steamTime == 0 || steamTimeLeft <= 0)
+            {
+                steamTime = TimeAligner.GetSteamTime();
+                steamTimeLeft = 30;
+                loadAccountInfo();
+            }
+            steamTimeLeft--;
+            pbTimeout.Value = steamTimeLeft;
         }
     }
 }
