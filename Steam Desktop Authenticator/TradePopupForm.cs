4c5aae839406ec5f43dec0d5c04fa0458ddb07d1
Pipe <pipernene@gmail.com>
2015-12-16 18:13:00 +0100

    Added TradePopupForm
    It will allow the user to either accept, deny or dismiss a Confirmation.

---
+++ Steam Desktop Authenticator/TradePopupForm.cs
@@ -0,0 +1,85 @@
+﻿using SteamAuth;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Steam_Desktop_Authenticator
+{
+    public partial class TradePopupForm : Form
+    {
+        public TradePopupForm(SteamGuardAccount account)
+        {
+            acc = account;
+            InitializeComponent();
+        }
+
+        private SteamGuardAccount acc;
+        public SteamGuardAccount Account
+        {
+            get { return acc; }
+            set { acc = value; }
+        }
+
+        private Confirmation confirm;
+        public Confirmation Confirmation
+        {
+            get { return confirm; }
+            set { confirm = value; }
+        }
+
+        private void TradePopupForm_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private bool deny2, accept2;
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (!accept2)
+            {
+                btnAccept.BackColor = Color.FromArgb(128, 255, 128);
+                accept2 = true;
+            }
+            else
+            {
+                acc.AcceptConfirmation(confirm);
+                Reset();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!accept2)
+            {
+                btnDeny.BackColor = Color.FromArgb(255, 255, 128);
+                deny2 = true;
+            }
+            else
+            {
+                acc.DenyConfirmation(confirm);
+                Reset();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Reset();
+        }
+
+        private void Reset()
+        {
+            deny2 = false;
+            accept2 = false;
+            btnAccept.BackColor = Color.FromArgb(192, 255, 192);
+            btnDeny.BackColor = Color.FromArgb(255, 255, 192);
+            this.Hide();
+        }
+    }
+}
