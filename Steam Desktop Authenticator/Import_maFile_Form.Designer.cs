7eb31a94dfafdccff33a1620a03efd4056a50faf
Angelus <hyt47@yahoo.com>
2015-12-16 06:20:15 +0200

    Update Import_maFile_Form.Designer.cs

--- Steam Desktop Authenticator/Import_maFile_Form.Designer.cs
+++ Steam Desktop Authenticator/Import_maFile_Form.Designer.cs
@@ -14,6 +14,7 @@ namespace Steam_Desktop_Authenticator
             this.txtBox = new System.Windows.Forms.TextBox();
             this.btnImport = new System.Windows.Forms.Button();
             this.btnCancel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // labelText
@@ -23,21 +24,21 @@ namespace Steam_Desktop_Authenticator
             this.labelText.ForeColor = System.Drawing.SystemColors.ControlText;
             this.labelText.Location = new System.Drawing.Point(13, 14);
             this.labelText.Name = "labelText";
-            this.labelText.Size = new System.Drawing.Size(309, 25);
+            this.labelText.Size = new System.Drawing.Size(319, 25);
             this.labelText.TabIndex = 0;
-            this.labelText.Text = "Enter your encription pass key if your .maFile is encripted";
+            this.labelText.Text = "Enter your encryption pass key if your .maFile is encripted";
             // 
             // txtBox
             // 
             this.txtBox.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtBox.Location = new System.Drawing.Point(15, 42);
             this.txtBox.Name = "txtBox";
-            this.txtBox.Size = new System.Drawing.Size(304, 33);
+            this.txtBox.Size = new System.Drawing.Size(307, 33);
             this.txtBox.TabIndex = 1;
             // 
             // btnImport
             // 
-            this.btnImport.Location = new System.Drawing.Point(16, 93);
+            this.btnImport.Location = new System.Drawing.Point(16, 129);
             this.btnImport.Name = "btnImport";
             this.btnImport.Size = new System.Drawing.Size(187, 28);
             this.btnImport.TabIndex = 2;
@@ -48,7 +49,7 @@ namespace Steam_Desktop_Authenticator
             // btnCancel
             // 
             this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnCancel.Location = new System.Drawing.Point(221, 93);
+            this.btnCancel.Location = new System.Drawing.Point(224, 129);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(98, 28);
             this.btnCancel.TabIndex = 3;
@@ -56,13 +57,25 @@ namespace Steam_Desktop_Authenticator
             this.btnCancel.UseVisualStyleBackColor = true;
             this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
+            // label1
+            // 
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(13, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(309, 40);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "If you import a .maFile encrypted, the file manifest.json needs to be next to it";
+            // 
             // Import_maFile_Form
             // 
             this.AcceptButton = this.btnImport;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btnCancel;
-            this.ClientSize = new System.Drawing.Size(334, 136);
+            this.ClientSize = new System.Drawing.Size(336, 171);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnImport);
             this.Controls.Add(this.txtBox);
@@ -84,5 +97,6 @@ namespace Steam_Desktop_Authenticator
         private System.Windows.Forms.TextBox txtBox;
         private System.Windows.Forms.Button btnImport;
         private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label label1;
     }
 }
