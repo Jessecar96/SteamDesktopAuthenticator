c35711846906b919369af78fa685aca782e1f221
Angelus <hyt47@yahoo.com>
2015-12-24 01:18:51 +0200

    Update SettingsForm.cs

--- Steam Desktop Authenticator/SettingsForm.cs
+++ Steam Desktop Authenticator/SettingsForm.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Windows.Forms;
 
 namespace Steam_Desktop_Authenticator
@@ -14,19 +14,56 @@ namespace Steam_Desktop_Authenticator
             // Get latest manifest
             manifest = Manifest.GetManifest(true);
 
+            // set GUI System Tray
+            string MinimiseToSystemTray_Val = manifest.MinimiseToSystemTray;
+            if (MinimiseToSystemTray_Val == "close") { radioButton1_SystemTray.Checked = true; }
+            if (MinimiseToSystemTray_Val == "minimise") { radioButton2_SystemTray.Checked = true; }
+            if (MinimiseToSystemTray_Val == "none") { radioButton3_SystemTray.Checked = true; }
+            if (MinimiseToSystemTray_Val == "hide") { radioButton4_SystemTray.Checked = true; }
+
+            // set GUI Confirmation Popup
             chkPeriodicChecking.Checked = manifest.PeriodicChecking;
             numPeriodicInterval.Value = manifest.PeriodicCheckingInterval;
-            chkCheckAll.Checked = manifest.CheckAllAccounts;
+
+            // set GUI Confirmation List btn
+            checkBoxConfirmationListBtn.Checked = manifest.ShowConfirmationListButton;
+
+            // set GUI Confirmation List btn
+            checkBoxAutoCheckForUpdates.Checked = manifest.AutoCheckForUpdates;
         }
 
-        private void btnSave_Click(object sender, EventArgs e)
+
+        private void SettingsOk_Click(object sender, EventArgs e)
         {
+            // System Tray
+            string SystemTrayValue = "close"; // default value
+            if (radioButton1_SystemTray.Checked) { SystemTrayValue = "close"; }
+            if (radioButton2_SystemTray.Checked) { SystemTrayValue = "minimise"; }
+            if (radioButton3_SystemTray.Checked) { SystemTrayValue = "none"; }
+            if (radioButton4_SystemTray.Checked) { SystemTrayValue = "hide"; }
+            manifest.MinimiseToSystemTray = SystemTrayValue;
+
+            // Confirmation Popup
             manifest.PeriodicChecking = chkPeriodicChecking.Checked;
             manifest.PeriodicCheckingInterval = (int)numPeriodicInterval.Value;
-            manifest.CheckAllAccounts = chkCheckAll.Checked;
-            manifest.Save();
 
+            // Confirmation List btn
+            manifest.ShowConfirmationListButton = checkBoxConfirmationListBtn.Checked;
+
+            // Updates
+            manifest.AutoCheckForUpdates = checkBoxAutoCheckForUpdates.Checked;
+
+            //save
+            manifest.Save();
+            //close
             this.Close();
         }
+
+        private void SettingsCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
     }
 }
