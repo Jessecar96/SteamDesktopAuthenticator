3bfc148e9ff0f7ae69a1abdfe4bec4a2d49f769a
Angelus <hyt47@yahoo.com>
2015-12-24 17:06:08 +0200

    Update TradePopupForm.Designer.cs

--- Steam Desktop Authenticator/TradePopupForm.Designer.cs
+++ Steam Desktop Authenticator/TradePopupForm.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Steam_Desktop_Authenticator
+namespace Steam_Desktop_Authenticator
 {
     partial class TradePopupForm
     {
@@ -34,14 +34,21 @@
             this.btnAccept = new System.Windows.Forms.Button();
             this.lblStatus = new System.Windows.Forms.Label();
             this.lblAccount = new System.Windows.Forms.Label();
+            this.label_Info = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.nextToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.backToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.xToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newConfirmationToolStripMenuItem = new System.Windows.Forms.ToolStripTextBox();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblDesc
             // 
             this.lblDesc.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDesc.Location = new System.Drawing.Point(12, 23);
+            this.lblDesc.Location = new System.Drawing.Point(0, 51);
             this.lblDesc.Name = "lblDesc";
-            this.lblDesc.Size = new System.Drawing.Size(142, 38);
+            this.lblDesc.Size = new System.Drawing.Size(182, 19);
             this.lblDesc.TabIndex = 1;
             this.lblDesc.Text = "trade description";
             this.lblDesc.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -50,9 +57,9 @@
             // 
             this.btnDeny.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
             this.btnDeny.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnDeny.Location = new System.Drawing.Point(12, 87);
+            this.btnDeny.Location = new System.Drawing.Point(12, 90);
             this.btnDeny.Name = "btnDeny";
-            this.btnDeny.Size = new System.Drawing.Size(69, 32);
+            this.btnDeny.Size = new System.Drawing.Size(76, 32);
             this.btnDeny.TabIndex = 2;
             this.btnDeny.Text = "Deny";
             this.btnDeny.UseVisualStyleBackColor = false;
@@ -62,9 +69,9 @@
             // 
             this.btnAccept.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
             this.btnAccept.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAccept.Location = new System.Drawing.Point(87, 87);
+            this.btnAccept.Location = new System.Drawing.Point(98, 90);
             this.btnAccept.Name = "btnAccept";
-            this.btnAccept.Size = new System.Drawing.Size(67, 32);
+            this.btnAccept.Size = new System.Drawing.Size(76, 32);
             this.btnAccept.TabIndex = 2;
             this.btnAccept.Text = "Accept";
             this.btnAccept.UseVisualStyleBackColor = false;
@@ -72,10 +79,11 @@
             // 
             // lblStatus
             // 
-            this.lblStatus.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblStatus.Location = new System.Drawing.Point(0, 61);
+            this.lblStatus.Font = new System.Drawing.Font("Segoe UI", 8.25F);
+            this.lblStatus.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lblStatus.Location = new System.Drawing.Point(0, 69);
             this.lblStatus.Name = "lblStatus";
-            this.lblStatus.Size = new System.Drawing.Size(166, 19);
+            this.lblStatus.Size = new System.Drawing.Size(182, 19);
             this.lblStatus.TabIndex = 3;
             this.lblStatus.Text = "status";
             this.lblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -83,31 +91,105 @@
             // lblAccount
             // 
             this.lblAccount.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblAccount.Location = new System.Drawing.Point(12, 0);
+            this.lblAccount.Location = new System.Drawing.Point(2, 31);
             this.lblAccount.Name = "lblAccount";
-            this.lblAccount.Size = new System.Drawing.Size(142, 23);
+            this.lblAccount.Size = new System.Drawing.Size(182, 19);
             this.lblAccount.TabIndex = 4;
             this.lblAccount.Text = "account name";
             this.lblAccount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label_Info
+            // 
+            this.label_Info.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Info.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label_Info.Location = new System.Drawing.Point(0, 124);
+            this.label_Info.Name = "label_Info";
+            this.label_Info.Size = new System.Drawing.Size(182, 14);
+            this.label_Info.TabIndex = 5;
+            this.label_Info.Text = "status";
+            this.label_Info.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.menuStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nextToolStripMenuItem,
+            this.backToolStripMenuItem,
+            this.xToolStripMenuItem,
+            this.newConfirmationToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Margin = new System.Windows.Forms.Padding(2, 0, 0, 0);
+            this.menuStrip1.MinimumSize = new System.Drawing.Size(184, 24);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(3);
+            this.menuStrip1.Size = new System.Drawing.Size(305, 25);
+            this.menuStrip1.Stretch = false;
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // nextToolStripMenuItem
+            // 
+            this.nextToolStripMenuItem.Name = "nextToolStripMenuItem";
+            this.nextToolStripMenuItem.Size = new System.Drawing.Size(54, 19);
+            this.nextToolStripMenuItem.Text = "Next >";
+            this.nextToolStripMenuItem.Click += new System.EventHandler(this.nextToolStripMenuItem_Click);
+            // 
+            // backToolStripMenuItem
+            // 
+            this.backToolStripMenuItem.Name = "backToolStripMenuItem";
+            this.backToolStripMenuItem.Size = new System.Drawing.Size(54, 19);
+            this.backToolStripMenuItem.Text = "< Back";
+            this.backToolStripMenuItem.Click += new System.EventHandler(this.backToolStripMenuItem_Click);
+            // 
+            // xToolStripMenuItem
+            // 
+            this.xToolStripMenuItem.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.xToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.xToolStripMenuItem.Name = "xToolStripMenuItem";
+            this.xToolStripMenuItem.Size = new System.Drawing.Size(26, 19);
+            this.xToolStripMenuItem.Text = "X";
+            this.xToolStripMenuItem.Click += new System.EventHandler(this.xToolStripMenuItem_Click);
+            // 
+            // newConfirmationToolStripMenuItem
+            // 
+            this.newConfirmationToolStripMenuItem.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.newConfirmationToolStripMenuItem.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.newConfirmationToolStripMenuItem.CausesValidation = false;
+            this.newConfirmationToolStripMenuItem.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.newConfirmationToolStripMenuItem.Margin = new System.Windows.Forms.Padding(5, 2, 0, 0);
+            this.newConfirmationToolStripMenuItem.Name = "newConfirmationToolStripMenuItem";
+            this.newConfirmationToolStripMenuItem.ReadOnly = true;
+            this.newConfirmationToolStripMenuItem.Size = new System.Drawing.Size(120, 17);
+            this.newConfirmationToolStripMenuItem.Text = "New Confirmation:";
             // 
             // TradePopupForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(166, 131);
-            this.Controls.Add(this.lblAccount);
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(184, 144);
+            this.Controls.Add(this.label_Info);
             this.Controls.Add(this.lblStatus);
             this.Controls.Add(this.btnAccept);
             this.Controls.Add(this.btnDeny);
             this.Controls.Add(this.lblDesc);
+            this.Controls.Add(this.lblAccount);
+            this.Controls.Add(this.menuStrip1);
             this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "TradePopupForm";
             this.Text = "New confirmation";
             this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.TradePopup_FormClosing);
             this.Load += new System.EventHandler(this.TradePopupForm_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -117,5 +199,11 @@
         private System.Windows.Forms.Button btnAccept;
         private System.Windows.Forms.Label lblStatus;
         private System.Windows.Forms.Label lblAccount;
+        private System.Windows.Forms.Label label_Info;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem nextToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem backToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem xToolStripMenuItem;
+        private System.Windows.Forms.ToolStripTextBox newConfirmationToolStripMenuItem;
     }
 }
