31b4ab972c9ef072ae42fc768a5712c99528ae5d
Angelus <hyt47@yahoo.com>
2015-12-24 17:04:54 +0200

    Update TradePopupForm.cs

--- Steam Desktop Authenticator/TradePopupForm.cs
+++ Steam Desktop Authenticator/TradePopupForm.cs
@@ -1,4 +1,4 @@
-﻿using SteamAuth;
+using SteamAuth;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -9,6 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace Steam_Desktop_Authenticator
 {
     public partial class TradePopupForm : Form
@@ -16,6 +17,10 @@ namespace Steam_Desktop_Authenticator
         private SteamGuardAccount acc;
         private List<Confirmation> confirms = new List<Confirmation>();
         private bool deny2, accept2;
+
+        public int TotalConfirmations = 0;
+        public int CurrentConfirmationNo = -1;
+        public int DisplayFirstSetOfConfirmations = 0;
 
         public TradePopupForm()
         {
@@ -26,7 +31,11 @@ namespace Steam_Desktop_Authenticator
         public SteamGuardAccount Account
         {
             get { return acc; }
-            set { acc = value; lblAccount.Text = acc.AccountName; }
+            set { acc = value;
+                lblAccount.Text = acc.AccountName;
+                lblStatus.Text = "Confirmation no. " + (1 + CurrentConfirmationNo).ToString();
+                label_Info.Text = "Total " + TotalConfirmations.ToString() + " confirmations waiting...";
+            }
         }
 
         public Confirmation[] Confirmation
@@ -52,8 +61,9 @@ namespace Steam_Desktop_Authenticator
             else
             {
                 lblStatus.Text = "Accepting...";
-                acc.AcceptConfirmation(confirms[0]);
-                confirms.RemoveAt(0);
+                acc.AcceptConfirmation(confirms[CurrentConfirmationNo]);
+                confirms.RemoveAt(CurrentConfirmationNo);
+                CurrentConfirmationNo = -1; // show again first confirmation
                 Reset();
             }
         }
@@ -69,14 +79,38 @@ namespace Steam_Desktop_Authenticator
             else
             {
                 lblStatus.Text = "Denying...";
-                acc.DenyConfirmation(confirms[0]);
-                confirms.RemoveAt(0);
+                acc.DenyConfirmation(confirms[CurrentConfirmationNo]);
+                confirms.RemoveAt(CurrentConfirmationNo);
+                CurrentConfirmationNo = -1; // show again first confirmation
                 Reset();
             }
         }
 
-        private void Reset()
+        private void Reset(string MoveDirection = "front")
         {
+            TotalConfirmations = confirms.Count;
+
+            // this will trigger the GUI to show next confirmation
+            if (MoveDirection == "front") {
+                if (TotalConfirmations > 1 && (TotalConfirmations - 1) > CurrentConfirmationNo)
+                {
+                    CurrentConfirmationNo++;
+                }
+                else {
+                    CurrentConfirmationNo = 0; // first confirmation
+                }
+            }
+            else if (MoveDirection == "back")
+            {
+                if (TotalConfirmations > 1 && CurrentConfirmationNo > 1)
+                {
+                    CurrentConfirmationNo--;
+                }
+                else {
+                    CurrentConfirmationNo = 0; // first confirmation
+                }
+            }
+
             deny2 = false;
             accept2 = false;
             btnAccept.BackColor = Color.FromArgb(192, 255, 192);
@@ -87,15 +121,61 @@ namespace Steam_Desktop_Authenticator
             lblAccount.Text = "";
             lblStatus.Text = "";
 
-            if (confirms.Count == 0)
+            if (TotalConfirmations == 0)
             {
                 this.Hide();
             }
             else
             {
-                lblDesc.Text = confirms[0].ConfirmationDescription;
+                lblDesc.Text = confirms[CurrentConfirmationNo].ConfirmationDescription;
             }
+            if (TotalConfirmations == 1) {
+                backToolStripMenuItem.Visible = false;
+                nextToolStripMenuItem.Visible = false;
+                newConfirmationToolStripMenuItem.Visible = true;
+            } else if (TotalConfirmations > 1) {
+                if (CurrentConfirmationNo == 0) { backToolStripMenuItem.Visible = false; } else { backToolStripMenuItem.Visible = true; }
+                if (CurrentConfirmationNo == (TotalConfirmations - 1)) { nextToolStripMenuItem.Visible = false; } else { nextToolStripMenuItem.Visible = true; }
+                newConfirmationToolStripMenuItem.Visible = false;
+            }
+                
         }
+
+
+        private void TradePopup_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                // show next confirmation
+                Reset();
+                DisplayFirstSetOfConfirmations = 0; // reset
+                this.Hide();
+                e.Cancel = true;
+            }
+            else
+            {
+                this.Close();
+            }
+
+        }
+
+        private void xToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DisplayFirstSetOfConfirmations = 0; // reset
+            this.Hide();
+        }
+
+        private void nextToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // show next confirmation
+            Reset();
+        }
+        private void backToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // show next confirmation
+            Reset("back");
+        }
+
 
         public void Popup()
         {
