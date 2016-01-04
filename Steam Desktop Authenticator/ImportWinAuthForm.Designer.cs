12ce86ea33aea9256ac1a7034a78a3c147c0b027
Tarun Kancharayan <tarun200897@gmail.com>
2016-01-04 13:20:07 +0530

    Improved GUI, Feature Update
    This commit brings the following changes.
    1. A better GUI
    2. Ability to load protected export zips
    3. Ability to select which account to import in case of multiple
    accounts in the export file.
    4. Removed dependency on System.Web which is not available in client
    profile versions of .NET.

--- Steam Desktop Authenticator/ImportWinAuthForm.Designer.cs
+++ Steam Desktop Authenticator/ImportWinAuthForm.Designer.cs
@@ -29,27 +29,39 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ImportWinAuthForm));
-            this.btnImport = new System.Windows.Forms.Button();
+            this.btnBrowse = new System.Windows.Forms.Button();
             this.btnCancel = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.listAccounts = new System.Windows.Forms.ListBox();
+            this.btnOk = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // btnImport
+            // btnBrowse
             // 
-            this.btnImport.Location = new System.Drawing.Point(128, 12);
-            this.btnImport.Name = "btnImport";
-            this.btnImport.Size = new System.Drawing.Size(187, 51);
-            this.btnImport.TabIndex = 4;
-            this.btnImport.Text = "Select .txt file to Import";
-            this.btnImport.UseVisualStyleBackColor = true;
-            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            this.btnBrowse.Location = new System.Drawing.Point(292, 54);
+            this.btnBrowse.Margin = new System.Windows.Forms.Padding(2);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(83, 23);
+            this.btnBrowse.TabIndex = 4;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
             // 
             // btnCancel
             // 
             this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnCancel.Location = new System.Drawing.Point(12, 12);
+            this.btnCancel.Location = new System.Drawing.Point(207, 259);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(2);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(110, 51);
+            this.btnCancel.Size = new System.Drawing.Size(82, 30);
             this.btnCancel.TabIndex = 5;
             this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
@@ -59,23 +71,136 @@
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(249, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "You can import your Steam account from WinAuth. ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(267, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "1. Select the zip/text file obtained from WinAuth export.";
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(15, 56);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.ReadOnly = true;
+            this.txtPath.Size = new System.Drawing.Size(274, 20);
+            this.txtPath.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Click";
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(48, 117);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 11;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(129, 122);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(208, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "and select the account you want to import.";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 88);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(156, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "2. Enter the password (if zip file)";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Enabled = false;
+            this.txtPassword.Location = new System.Drawing.Point(175, 85);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(201, 20);
+            this.txtPassword.TabIndex = 14;
+            // 
+            // listAccounts
+            // 
+            this.listAccounts.FormattingEnabled = true;
+            this.listAccounts.Location = new System.Drawing.Point(15, 146);
+            this.listAccounts.Name = "listAccounts";
+            this.listAccounts.Size = new System.Drawing.Size(361, 108);
+            this.listAccounts.TabIndex = 15;
+            this.listAccounts.SelectedIndexChanged += new System.EventHandler(this.listAccounts_SelectedIndexChanged);
+            // 
+            // btnOk
+            // 
+            this.btnOk.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnOk.Location = new System.Drawing.Point(293, 260);
+            this.btnOk.Margin = new System.Windows.Forms.Padding(2);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(82, 30);
+            this.btnOk.TabIndex = 16;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            // 
             // ImportWinAuthForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(327, 75);
+            this.ClientSize = new System.Drawing.Size(388, 301);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.listAccounts);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnCancel);
-            this.Controls.Add(this.btnImport);
+            this.Controls.Add(this.btnBrowse);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "ImportWinAuthForm";
             this.Text = "Import from WinAuth";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.Button btnBrowse;
         private System.Windows.Forms.Button btnCancel;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.ListBox listAccounts;
+        private System.Windows.Forms.Button btnOk;
     }
 }
--- Steam Desktop Authenticator/ImportWinAuthForm.cs
+++ Steam Desktop Authenticator/ImportWinAuthForm.cs
@@ -1,6 +1,9 @@
-﻿using Newtonsoft.Json;
+﻿using ICSharpCode.SharpZipLib.Core;
+using ICSharpCode.SharpZipLib.Zip;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
@@ -14,7 +17,8 @@ namespace Steam_Desktop_Authenticator
 {
     public partial class ImportWinAuthForm : Form
     {
-        SteamAuth.SteamGuardAccount steamAccount;
+        SteamAuth.SteamGuardAccount[] steamAccounts;
+        SteamAuth.SteamGuardAccount selectedAccount;
         public ImportWinAuthForm()
         {
             InitializeComponent();
@@ -25,41 +29,189 @@ namespace Steam_Desktop_Authenticator
             this.Close();
         }
 
-        private void btnImport_Click(object sender, EventArgs e)
+        private void btnBrowse_Click(object sender, EventArgs e)
         {
             // File Dialog options
-            openFileDialog1.Filter = "WinAuth Export (.txt)|*.txt|All Files (*.*)|*.*";
+            openFileDialog1.Filter = "WinAuth Protected Export (.zip)|*.zip|WinAuth Export (.txt)|*.txt|All Files (*.*)|*.*";
             openFileDialog1.FilterIndex = 1;
             openFileDialog1.Multiselect = false;
 
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 var path = openFileDialog1.FileName;
-                var contents = File.ReadAllText(path);
-
-                // WinAuth export is similar to http query paramters
-                var collection = System.Web.HttpUtility.ParseQueryString(contents);
-
-                // The data assigned to data is actually valid json
-                var jsondata = collection.GetValues("data")[0];
-                steamAccount = JsonConvert.DeserializeObject<SteamAuth.SteamGuardAccount>(jsondata);
-                
-                // WinAuth stores DeviceID outside of the data json
-                steamAccount.DeviceID = collection.GetValues("deviceid")[0];
-                LoginAccount();
+                txtPassword.Enabled = Path.GetExtension(path) == ".zip";
+                txtPath.Text = path;
             }
         }
         
-        // Same as PhoneExtractForm's Login method since both have the same data at this stage.
+        // Similar to PhoneExtractForm's LoginAccount method since both have the same data at this stage.
         private void LoginAccount()
         {
-            MessageBox.Show("Account extracted succesfully! Please login to it.");
             LoginForm login = new LoginForm();
-            login.androidAccount = steamAccount;
+            login.androidAccount = selectedAccount;
             login.loginFromAndroid = true;
-            login.SetUsername(steamAccount.AccountName);
+            login.SetUsername(selectedAccount.AccountName);
             login.ShowDialog();
             this.Close();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            var path = txtPath.Text;
+            List<SteamAuth.SteamGuardAccount> accountList = new List<SteamAuth.SteamGuardAccount>();
+            string[] contents;
+            var ext = Path.GetExtension(path);
+            if (ext == ".txt")
+            {
+                contents = File.ReadAllLines(path);
+            }
+            else if(ext == ".zip")
+            {
+                ZipFile zf = null;
+                List<string> contentsList = new List<string>();
+                try
+                {
+                    FileStream fs = File.OpenRead(path);
+                    zf = new ZipFile(fs);
+                    if (!String.IsNullOrEmpty(txtPassword.Text))
+                    {
+                        zf.Password = txtPassword.Text;     // AES encrypted entries are handled automatically
+                    }
+                    else
+                    {
+                        return;
+                    }
+                    foreach (ZipEntry zipEntry in zf)
+                    {
+                        if (!zipEntry.IsFile)
+                        {
+                            continue;           // Ignore directories
+                        }
+                        String entryFileName = zipEntry.Name;
+                        // to remove the folder from the entry:- entryFileName = Path.GetFileName(entryFileName);
+                        // Optionally match entrynames against a selection list here to skip as desired.
+                        // The unpacked length is available in the zipEntry.Size property.
+
+                        // Look for the winauth text file.
+                        if(!entryFileName.Contains("winauth"))
+                        {
+                            continue;
+                        }
+
+                        Stream zipStream;
+                        try
+                        {
+                           
+                            zipStream = zf.GetInputStream(zipEntry);
+                        }
+                        catch (Exception)
+                        {
+                            MessageBox.Show("Incorrect Password");
+                            return;
+                        }
+
+                        // Read the contents from the zip stream
+                        // The "using" will close the stream even if an exception occurs.
+                        using (StreamReader reader = new StreamReader(zipStream))
+                        {
+                            while (!reader.EndOfStream)
+                            {
+                                contentsList.Add(reader.ReadLine());
+                            }
+                        }
+                    }
+                }
+                finally
+                {
+                    if (zf != null)
+                    {
+                        zf.IsStreamOwner = true; // Makes close also shut the underlying stream
+                        zf.Close(); // Ensure we release resources
+                    }
+                }
+                contents = contentsList.ToArray();
+            }
+            else
+            {
+                return;
+            }
+
+            foreach (var line in contents)
+            {
+                // Checking if it's a steam export
+                if (line.Contains("steam"))
+                {
+                    // WinAuth export is similar to http query paramters
+                    var collection = ParseQueryString(line);
+
+                    // The data assigned to data is actually valid json
+                    var jsondata = collection.GetValues("data")[0];
+                    var account = JsonConvert.DeserializeObject<SteamAuth.SteamGuardAccount>(jsondata);
+
+                    // WinAuth stores DeviceID outside of the data json
+                    account.DeviceID = collection.GetValues("deviceid")[0];
+
+                    accountList.Add(account);
+                    listAccounts.Items.Add(account.AccountName);
+                }
+            }
+            steamAccounts = accountList.ToArray();
+        }
+
+        // Function copied from WinAuth code - WinAuth/WinAuthHelper.cs.
+        /// <summary>
+        /// Our own version of HttpUtility.ParseQueryString so we can remove the reference to System.Web
+        /// which is not available in client profile.
+        /// </summary>
+        /// <param name="qs">string query string</param>
+        /// <returns>collection of name value pairs</returns>
+        public static NameValueCollection ParseQueryString(string qs)
+        {
+            NameValueCollection pairs = new NameValueCollection();
+
+            // ignore blanks and remove initial "?"
+            if (string.IsNullOrEmpty(qs) == true)
+            {
+                return pairs;
+            }
+            if (qs.StartsWith("?") == true)
+            {
+                qs = qs.Substring(1);
+            }
+
+            // get each a=b&... key-value pair
+            foreach (string p in qs.Split('&'))
+            {
+                string[] keypair = p.Split('=');
+                string key = keypair[0];
+                string v = (keypair.Length >= 2 ? keypair[1] : null);
+                if (string.IsNullOrEmpty(v) == false)
+                {
+                    // decode (without using System.Web)
+                    string newv;
+                    while ((newv = Uri.UnescapeDataString(v)) != v)
+                    {
+                        v = newv;
+                    }
+                }
+                pairs.Add(key, v);
+            }
+
+            return pairs;
+        }
+
+        private void listAccounts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var accountName = (string)listAccounts.SelectedItem;
+            for (int i = 0; i < steamAccounts.Length; i++)
+            {
+                var account = steamAccounts[i];
+                if (account.AccountName == (string)listAccounts.Items[listAccounts.SelectedIndex])
+                {
+                    selectedAccount = account;
+                    break;
+                }
+            }
         }
     }
 }
