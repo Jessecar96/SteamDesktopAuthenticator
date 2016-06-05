aa1d1ff18b135d0268fae44a30225499166b1164
Алексей Скляров <sufir@lightsoft.ru>
2016-06-05 12:08:33 +0300

    Autoconfirm for all acounts fix

--- Steam Desktop Authenticator/MainForm.cs
+++ Steam Desktop Authenticator/MainForm.cs
@@ -423,7 +423,7 @@ namespace Steam_Desktop_Authenticator
                 {
                     try
                     {
-                        Confirmation[] tmp = await currentAccount.FetchConfirmationsAsync();
+                        Confirmation[] tmp = await acc.FetchConfirmationsAsync();
                         foreach (var conf in tmp)
                         {
                             if (conf.ConfType == Confirmation.ConfirmationType.MarketSellTransaction && manifest.AutoConfirmMarketTransactions)
@@ -437,7 +437,7 @@ namespace Steam_Desktop_Authenticator
                     catch (SteamGuardAccount.WGTokenInvalidException)
                     {
                         lblStatus.Text = "Refreshing session";
-                        await currentAccount.RefreshSessionAsync(); //Don't save it to the HDD, of course. We'd need their encryption passkey again.
+                        await acc.RefreshSessionAsync(); //Don't save it to the HDD, of course. We'd need their encryption passkey again.
                         lblStatus.Text = "";
                     }
                     catch (WebException)
