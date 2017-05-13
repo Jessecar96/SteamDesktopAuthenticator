6f24fea50ad9be1ecca664cbfdae3c37b8ca961f
Jesse Cardone <jessecar96@gmail.com>
2017-05-13 01:26:40 -0400

    Changed default button to copy

--- Steam Desktop Authenticator/MainForm.Designer.cs
+++ Steam Desktop Authenticator/MainForm.Designer.cs
@@ -436,7 +436,7 @@
             // 
             // MainForm
             // 
-            this.AcceptButton = this.btnSteamLogin;
+            this.AcceptButton = this.btnCopy;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(351, 433);
