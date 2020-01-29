28e0160c908fa35ee55e3f03e56221412e7269bd
Jesse Cardone <jessecar96@gmail.com>
2020-01-29 14:59:16 -0500

    Update prepare-release.bat

--- prepare-release.bat
+++ prepare-release.bat
@@ -6,8 +6,9 @@ del /q "Steam Desktop Authenticator\bin\Release\ffmpegsumo.dll"
 del /q "Steam Desktop Authenticator\bin\Release\d3dcompiler_43.dll"
 del /q "Steam Desktop Authenticator\bin\Release\widevinecdmadapter.dll"
 
-REM Remove CEF locale files
+REM Remove CEF directories
 RD /S /Q "Steam Desktop Authenticator\bin\Release\locales\"
+RD /S /Q "Steam Desktop Authenticator\bin\Release\swiftshader\"
 
 REM Remove CEF debug files
 del /q "Steam Desktop Authenticator\bin\Release\CefSharp.BrowserSubprocess.Core.pdb"
@@ -21,6 +22,7 @@ del /q "Steam Desktop Authenticator\bin\Release\Newtonsoft.Json.xml"
 del /q "Steam Desktop Authenticator\bin\Release\CefSharp.xml"
 del /q "Steam Desktop Authenticator\bin\Release\CefSharp.WinForms.xml"
 del /q "Steam Desktop Authenticator\bin\Release\CefSharp.Core.xml"
+del /q "Steam Desktop Authenticator\bin\Release\CommandLine.xml"
 
 REM Remove ClickOnce app
 del /q "Steam Desktop Authenticator\bin\Release\Steam Desktop Authenticator.application"
