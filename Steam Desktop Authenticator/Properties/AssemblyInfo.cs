097b75b86d846ea7af8278258d6f507069ea53a3
Jesse Cardone <jessecar96@gmail.com>
2024-10-15 16:15:38 -0700

    Version 1.0.15

--- Steam Desktop Authenticator/Properties/AssemblyInfo.cs
+++ Steam Desktop Authenticator/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.14")]
-[assembly: AssemblyFileVersion("1.0.14")]
+[assembly: AssemblyVersion("1.0.15")]
+[assembly: AssemblyFileVersion("1.0.15")]
 
 [assembly: AssemblyMetadata("SquirrelAwareVersion", "1")]
